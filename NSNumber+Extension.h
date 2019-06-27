637dced314ab7396370580d33e2d110d00e75ce6
Choi <365473321@qq.com>
2019-06-27 17:06:03 +0800

    新增NSNumber分类

---
+++ NSNumber+Extension.h
@@ -0,0 +1,19 @@
+//
+//  NSNumber+Extension.h
+//  DepositExpress
+//
+//  Created by Ori on 27/6/2019.
+//  Copyright © 2019 Ori. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSNumber (Extension)
+
+@property (nonatomic, readonly) NSString *decimalString;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NSNumber+Extension.m
@@ -0,0 +1,19 @@
+//
+//  NSNumber+Extension.m
+//  DepositExpress
+//
+//  Created by Ori on 27/6/2019.
+//  Copyright © 2019 Ori. All rights reserved.
+//
+
+#import "NSNumber+Extension.h"
+
+@implementation NSNumber (Extension)
+
+- (NSString *)decimalString {
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    formatter.numberStyle = NSNumberFormatterDecimalStyle;
+    return [formatter stringFromNumber:self];
+}
+
+@end
